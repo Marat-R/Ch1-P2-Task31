db75c49285f20e76d5ac45e3075fc86e592f9407
Marat-R <you@example.com>
2019-12-26 09:27:59 +0600

    first commit

---
+++ Task31.py
@@ -0,0 +1,19 @@
+lines = 0
+words = 0
+letters = 0
+
+for line in open("txtTask31.txt", "r"):
+    lines += 1
+    letters += len(line)
+    pos = "out"
+
+    for letter in line:
+        if letter != " " and pos == "out":
+            words += 1
+            pos = "in"
+        elif letter == " ":
+            pos = "out"
+
+print("Lines: ", lines)
+print("Words: ", words)
+print("Letters: ", letters)
